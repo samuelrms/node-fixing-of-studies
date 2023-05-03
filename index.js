972bdeebd3ece7d4be6c23b872232f1d992678ec
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-03 13:37:58 -0300

    Fix: add route user

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const hostname = '192.168.100.26';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  fs.readFile('index.html', (err, data) => {
-    res.writeHead(200, { 'Content-type': 'text/html' });
-    res.write(data);
 
+  if (req.url == '/hello-user') {
+    fs.readFile('index.html', (err, data) => {
+      res.writeHead(200, { 'Content-type': 'text/html' });
+      res.write(data);
+      return res.end()
+    })
+  } else {
     return res.end()
-  })
+  }
 })
 
 server.listen(port, hostname, () => {
