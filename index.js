3580b2418a7f47a3fed96bba946bb837e87dd0cc
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-03 14:01:12 -0300

    Feat: Creating functions for inserting new created files

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-const http = require('http');
 const fs = require('fs');
 
-const hostname = '192.168.100.26';
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-
-  if (req.url == '/hello-user') {
-    fs.readFile('index.html', (err, data) => {
-      res.writeHead(200, { 'Content-type': 'text/html' });
-      res.write(data);
-      return res.end()
-    })
-  } else {
-    return res.end()
-  }
+/*Create new file */
+fs.writeFile('user.txt', 'Guest user test', (err, data) => {
+  if (err) throw err;
+  console.log('The file was created correctly');
 })
 
-server.listen(port, hostname, () => {
-  console.log('Servidor está rodando')
-});
+/*Create new file and insert content after existing one (appendFile)*/
+fs.appendFile('user.txt', '\nAdditional content', (err, data) => {
+  if (err) throw err;
+  console.log('Change made with dexterity');
+})
