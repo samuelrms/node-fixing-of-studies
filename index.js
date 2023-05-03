2d00450a866f5a4dc752224afb05f78cee9256e9
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-03 13:23:08 -0300

    Feat: exemple create server

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const http = require('http')
+const http = require('http');
 
-const hostname = '192.168.100.26'
-const port = 8080
+const hostname = '192.168.100.26';
+const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
-  res.end('Hello user')
+  res.end('Hello user');
 })
 
 server.listen(port, hostname, () => {
   console.log('Servidor está rodando')
-})
+});
