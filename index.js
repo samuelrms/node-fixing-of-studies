09cf2b529a80a8597cb83583f4a33c0175a3e030
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-03 14:10:57 -0300

    Join functions to add new file and add index.html page to new file

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const fs = require('fs');
 const hostname = '192.168.100.26';
 const port = 8080;
 
-const server = http.createServer((req, res) => {
+/*Create new file */
+fs.writeFile('copyIndex.txt', 'Guest user test', (err, data) => {
+  if (err) throw err;
+  console.log('The file was created correctly');
+})
 
+const server = http.createServer((req, res) => {
   if (req.url == '/hello-user') {
     fs.readFile('index.html', (err, data) => {
       res.writeHead(200, { 'Content-type': 'text/html' });
       res.write(data);
+
+      /*Create new file and insert content after existing one (appendFile)*/
+      fs.appendFile('copyIndex.txt', `\n\n${data}`, (err, data) => {
+        if (err) throw err;
+        console.log('Change made with dexterity');
+      })
+
       return res.end()
     })
   } else {
