30730f39fc667a5d42dda491d5a23a8783f6a776
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-03 21:11:29 -0300

    Examples of capturing what the user types in the console

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const { log } = require('console');
-const fs = require('fs')
+const readLine = require('readline')
 
-/**@info Function to delete the selected file */
-const deleteFile = () => fs.unlink('hello-user.txt', (err) => {
-  log('File deleted')
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Whats your name? \n', (name) => {
+  log(`\nHis name is: \n${name}\n`);
+
+  rl.question('How old are you?\n', (old) => {
+    log(`\n Your age is: \n${old}\n`)
+
+    rl.question("What's your height?\n", (height) => {
+      log(`\nYour height is: \n${height}\n`)
+
+      log(`\n Your name is ${name}, you are ${old} years old and your height is ${height}\n`)
+    })
+  })
+
 })
 
-deleteFile()
-
-/**@info Function to rename the selected file*/
-const renameFile = () => fs.rename('hello-user.txt', 'hello-word.txt', (err) => {
-  log('Renamed file')
+rl.on('close', () => {
+  log('\nGoodbye!');
+  process.exit(0)
 })
-
-renameFile()
