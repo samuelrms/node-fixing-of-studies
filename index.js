50e098d33335a960f813eb17e6d54aefb8e86fc8
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-04 01:42:46 -0300

    Feat: create todo card

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const { log } = require('console');
-const customModel = require('./modelsExemple')
-const ExempleClass = require('./models')
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-const exempleClass = new ExempleClass()
+const app = express()
 
-log(customModel.name)
-log(customModel.age, '\n')
-log(customModel.obj, '\n')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-log(customModel.arr, '\n')
-log(customModel.soma(1, 1), '\n')
-customModel.customModels()
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'))
 
-exempleClass.test()
-log(`\n${exempleClass.name}`)
+let tasks = [
+  'Análise e Levantamento de Requisitos de Software',
+  'Criação de Aplicações e Sistemas'
+]
+
+app.post('/', (req, res) => {
+  tasks.push(req.body.task)
+
+  res.render('index', { nome: 'Samuel', tasksList: tasks })
+})
+
+app.get('/', (_, res) => {
+  res.render('index', { nome: 'Samuel', tasksList: tasks })
+})
+
+app.get('/delete/:id', (req, res) => {
+  tasks = tasks.filter((data, i) => {
+    if (i != req.params.id) return data
+  })
+
+  res.render('index', { nome: 'Samuel', tasksList: tasks }, () => {
+    res.redirect('/')
+  })
+})
+
+app.listen(8080, () => {
+  log("start server")
+})
