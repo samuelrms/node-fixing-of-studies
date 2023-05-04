7742115444b79f3f05acd7aa22bb6d2429516690
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-03 21:24:09 -0300

    feat: install nodemon

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
 const { log } = require('console');
-const readLine = require('readline')
 
-const rl = readLine.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Whats your name? \n', (name) => {
-  log(`\nHis name is: \n${name}\n`);
-
-  rl.question('How old are you?\n', (old) => {
-    log(`\n Your age is: \n${old}\n`)
-
-    rl.question("What's your height?\n", (height) => {
-      log(`\nYour height is: \n${height}\n`)
-
-      log(`\n Your name is ${name}, you are ${old} years old and your height is ${height}\n`)
-    })
-  })
-
-})
-
-rl.on('close', () => {
-  log('\nGoodbye!');
-  process.exit(0)
-})
+log('\nInstall nodemon')
+log('\nnpm install -g nodemon\n')
+log('||\n')
+log('npm i nodemon\n')
+log('To run nodemon just open the terminal in your project folder, either by vscode or by prompt and type "nodemon"\n')
